d927fa7f77e10dbd5df15d9f677862f1556aafa9
Cnbados55380 <c.nbados@alumnos.upm.es>
2024-03-27 19:40:57 +0100

    Creación clase Pieza

---
+++ src/Pieza.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "ETSIDI.h"
+
+using namespace ETSIDI;
+
+class Pieza {
+private:
+	Sprite sprite;
+	int value;
+public:
+	
+	Pieza(Sprite& sprite, Vector2D posicion, int val);
+
+
+};
