66a32c15233662cf5784dbd272fbd40a8c570257
luciaaantolin <lucia.apulido@alumnos.upm.es>
2024-03-27 18:21:29 +0100

    Tamaño del tablero arreglado

--- src/Tablero.cpp
+++ src/Tablero.cpp
@@ -12,18 +12,25 @@ Tablero::Tablero() {
 }
 
 void Tablero::dibujar() const {
-    /*glMatrixMode(GL_PROJECTION); // Cambia la matriz actual a la matriz de proyección
-    glLoadIdentity(); // Resetea la matriz de proyección a la identidad
-    // Establece una proyección ortográfica
-    glOrtho(-683, 683, -384, 384, 1, -1);*/
+    
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if ((i + j) % 2 == 0) {
+                glColor3ub(253, 253, 180); // marrón claro
+            }
+            else {
+                glColor3ub(150, 55, 0); // marrón oscuro 
+            }
+            glBegin(GL_POLYGON);
+            glVertex3f(10 * i - 40, 10 * j - 41, 0); //Aumenta el tamaño del tablero multiplicando por 10, 
+            //ajusta las coordenadas para centrar el tablero en el centro
+            glVertex3f(10 * i - 40, 10 * (j + 1) - 41, 0);
+            glVertex3f(10 * (i + 1) - 40, 10 * (j + 1) - 41, 0);
+            glVertex3f(10 * (i + 1) - 40, 10 * j - 41, 0);
+            glEnd();
+        }
 
-    //glMatrixMode(GL_MODELVIEW); // Cambia la matriz actual de vuelta a la matriz de modelo/vista
-
-    gluLookAt(5.5, 5.5, 12,  // posicion del ojo
-        5.5, 5.5, 0.0,      // hacia que punto mira  (0,0,0)
-        0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)
-
-    // Dibujar el tablero utilizando OpenGL
+    /*// Dibujar el tablero utilizando OpenGL
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             if ((i + j) % 2 == 0) {
@@ -44,22 +51,9 @@ void Tablero::dibujar() const {
                 glVertex3f(i + 2.5, j + 2.5, 0);
                 glVertex3f(i + 2.5, j + 1.5, 0);
                 glEnd();
-            }
-            /*for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    if ((i + j) % 2 == 0) {
-                        glColor3ub(253, 246, 227); // Un marrón claro más ajustado
-                    }
-                    else {
-                        glColor3ub(121, 68, 59); // Un marrón oscuro más ajustado
-                    }
-                    glBegin(GL_QUADS); // Usar GL_QUADS para simplicidad
-                    glVertex3f(i, j, 0);
-                    glVertex3f(i, j + 1, 0);
-                    glVertex3f(i + 1, j + 1, 0);*/
+            }*/
 
-            glEnable(GL_LIGHTING);
-        }
+        glEnable(GL_LIGHTING);
     }
 }
 /*
